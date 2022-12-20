27d128ee7711c9bef2376886ebd5a5a0fb420296
Luisa <marialuisamanzanom@gmail.com>
2022-12-20 00:17:44 -0600

    ejercicio 3 corregido

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,8 +1,9 @@
 let numeroFactorial = Number(prompt("Ingresa un número para conocer su factorial"));
+let resultado = 1;
 function calcularFactorial() {
     if(numeroFactorial >= 0){
-            for (let i = 0; i <= numeroFactorial; i++){
-                resultado = i * i;
+            for (let i = 1; i <= numeroFactorial; i++){
+                resultado = resultado * i;
             }
             alert("El número factorial del valor ingresado es de: " + resultado);
     }else {

b65719d2ef6fff1b7500607b1f8037e051dbd189
Luisa <marialuisamanzanom@gmail.com>
2022-12-22 11:39:40 -0600

    ejercicio 6

---
+++ ejercicio6/Ejercicio6palabraInversa/src/ejercicio6palabras/PalabraInversa.java
@@ -0,0 +1,20 @@
+package ejercicio6palabras;
+
+import java.util.Scanner;
+
+public class PalabraInversa {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduce una palabra o frase que desees invertir");
+		String palabra = scanner.nextLine();
+		StringBuilder palabraInv = new StringBuilder(palabra);
+		
+		for (int i = palabraInv.length()-1; i>=0; i--) {
+			System.out.println(palabra.charAt(i));
+		}
+	}
+
+}

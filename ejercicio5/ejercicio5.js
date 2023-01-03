8c5dd2e33af5bb16866a0eb4486f44dcf07690d9
Luisa Manzano <115746809+LuisaAppleTree@users.noreply.github.com>
2023-01-03 12:32:10 -0600

    Update ejercicio5.js

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -16,15 +16,37 @@
 // }
 // adivinarNum();
 
-let numAdivino = Number(prompt("Ingresa el número que crees que debes adivinar"));
-let arregloNumAdivinados = [];
-let valorNum = 0;
-while ((numAdivino >= 0) && (numAdivino <= 100)) {
-    valorNum = numAdivino
-    arregloNumAdivinados.add(valorNum);
-    if (valorNum == 5) {
-        numAdivino = false;
-        arregloNumAdivinados.remove(arregloNumAdivinados.size() -1);
-    }
+
+
+// let numAdivino = Number(prompt("Ingresa el número que crees que debes adivinar"));
+// let arregloNumAdivinados = [];
+// let valorNum = 0;
+// while ((numAdivino >= 0) && (numAdivino <= 100)) {
+//     valorNum = numAdivino
+//     arregloNumAdivinados.add(valorNum);
+//     if (valorNum == 5) {
+//         numAdivino = false;
+//         arregloNumAdivinados.remove(arregloNumAdivinados.size() -1);
+//     }
+// }
+// console.log( "haz adivinado el número secreto! que es: " + 5 + "tus intentos por adivinar fueron: " + arregloNumAdivinados);
+
+
+
+
+let max = 100;
+let min = 1;
+let numerom = Math.random() * (max - min) + min;
+numerom = parseInt(numerom);
+let usuario = prompt("Adivina el número secreto entre 1 y 100. Ingresa tu respuesta.");
+
+while (true) {
+  usuario = prompt("Ups, el número secreto es incorrecto, vuelve a intentarlo.");
+
+  if (usuario == numerom) {
+    alert("HAS GANADO");
+    break;
+  } else if (usuario == 0) {
+    break;
+  }
 }
-console.log( "haz adivinado el número secreto! que es: " + 5 + "tus intentos por adivinar fueron: " + arregloNumAdivinados);
